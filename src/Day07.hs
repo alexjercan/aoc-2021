7bb19cf923f0731d3cc24d76c8202543f38c6469
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-12-07 08:08:00 +0200

    added solution for day 7

--- src/Day07.hs
+++ src/Day07.hs
@@ -1,10 +1,45 @@
 module Day07 where
 
+import Data.List.Split (splitOn)
+import Data.List (sort)
+
+parseContent :: String -> [Int]
+parseContent = map read . splitOn ","
+
+cost1 :: [Int] -> Int
+cost1 = sum
+
+cost2 :: [Int] -> Int
+cost2 = sum . map (\x -> x * (x + 1) `div` 2)
+
+average :: [Int] -> Int
+average xs = round $ fromIntegral (sum xs) / (fromIntegral n :: Float)
+    where n = length xs
+
+median :: [Int] -> Int
+median xs
+    | n `mod` 2 == 1 = xs' !! m
+    | otherwise      = average [xs' !! m , xs' !! (m - 1)]
+    where xs' = sort xs
+          n = length xs
+          m = n `div` 2
+
+solution1 :: [Int] -> Int
+solution1 xs = sum $ map (\x -> abs $ x - med) xs
+    where med = median xs
+
+solution2 :: [Int] -> Int
+solution2 xs = minimum costs
+    where avg = average xs
+          med = median xs
+          searchInt = [min avg med .. max avg med]
+          costs = map (\i -> cost2 $ map (\x -> abs (x - i)) xs) searchInt
+
 solve1 :: String -> Int
-solve1 content = undefined
+solve1 = solution1 . parseContent
 
 solve2 :: String -> Int
-solve2 content = undefined
+solve2 = solution2 . parseContent
 
 solve :: String -> IO ()
 solve content = do
--- test/Day07Test.hs
+++ test/Day07Test.hs
@@ -5,15 +5,13 @@ module Day07Test where
 import Day07
 import Test.Framework
 
-sampleInput :: IO String
-sampleInput = readFile "input/day07/sample_input.txt"
+sampleInput :: String
+sampleInput = "16,1,2,0,4,2,7,1,2,14\n"
 
 test_solve1 :: IO ()
 test_solve1 = do
-    content <- sampleInput
-    assertEqual 0 $ solve1 content
+    assertEqual 37 $ solve1 sampleInput
 
 test_solve2 :: IO ()
 test_solve2 = do
-    content <- sampleInput
-    assertEqual 0 $ solve2 content
+    assertEqual 168 $ solve2 sampleInput
