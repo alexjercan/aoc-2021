8f77e903d5978a9c2961977cfdd09095d4e1e2ef
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-12-07 19:56:06 +0200

    updated day 7 according to a papper on reddit

--- src/Day07.hs
+++ src/Day07.hs
@@ -10,30 +10,32 @@ cost1 :: [Int] -> Int
 cost1 = sum
 
 cost2 :: [Int] -> Int
-cost2 = sum . map (\x -> x * (x + 1) `div` 2)
+cost2 = (`quot` 2) . sum . map (\x -> x * (x + 1))
 
-average :: [Int] -> Int
-average xs = round $ fromIntegral (sum xs) / (fromIntegral n :: Float)
+diffAbs :: Num b => b -> [b] -> [b]
+diffAbs m = map (abs . flip (-)m)
+
+average :: (Fractional t, Integral a) => (t -> a) -> [a] -> a
+average roundF xs = roundF $ fromIntegral (sum xs) / fromIntegral n
     where n = length xs
 
-median :: [Int] -> Int
-median xs
+median :: (Fractional t, Integral a) => (t -> a) -> [a] -> a
+median roundF xs
     | n `mod` 2 == 1 = xs' !! m
-    | otherwise      = average [xs' !! m , xs' !! (m - 1)]
+    | otherwise      = average roundF [xs' !! m, xs' !! (m - 1)]
     where xs' = sort xs
           n = length xs
           m = n `div` 2
 
 solution1 :: [Int] -> Int
-solution1 xs = sum $ map (\x -> abs $ x - med) xs
-    where med = median xs
+solution1 xs = min (cost1 $ diffAbs med1 xs) (cost2 $ diffAbs med2 xs)
+    where med1 = median floor xs
+          med2 = median ceiling xs
 
 solution2 :: [Int] -> Int
-solution2 xs = minimum costs
-    where avg = average xs
-          med = median xs
-          searchInt = [min avg med .. max avg med]
-          costs = map (\i -> cost2 $ map (\x -> abs (x - i)) xs) searchInt
+solution2 xs = min (cost2 $ diffAbs avg1 xs) (cost2 $ diffAbs avg2 xs)
+    where avg1 = average floor xs
+          avg2 = average ceiling xs
 
 solve1 :: String -> Int
 solve1 = solution1 . parseContent
