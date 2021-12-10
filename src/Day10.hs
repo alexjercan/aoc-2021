b40573453a4c078a2da8a865ff06d7c7ea462d2f
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-12-10 10:51:29 +0200

    added day 10

--- src/Day10.hs
+++ src/Day10.hs
@@ -8,8 +8,6 @@ import Data.List (sort)
 parseContent :: String -> [String]
 parseContent = lines
 
-type Eval = State (String, String)
-
 mappingP :: Char -> Char
 mappingP '(' = ')'
 mappingP '[' = ']'
@@ -31,37 +29,35 @@ costMap '{' = 3
 costMap '<' = 4
 costMap _ = undefined
 
-checkLine :: Eval Int
-checkLine = do
-    (xs, ss) <- get
-    case xs of
-      (y:ys) -> if y `elem` "([{<"
-                   then put (ys, y:ss) >> checkLine
-                   else case ss of
-                          (z:zs) -> if mappingP z == y
-                                       then put (ys, zs) >> checkLine
-                                       else return $ errorMap y
-                          _ -> return $ errorMap y
-      _ -> return 0
+type Eval = State String
+
+checkLine :: String -> Eval Int
+checkLine [] = return 0
+checkLine (x:xs)  = do
+    ss <- get
+    if x `elem` "([{<"
+       then put (x:ss) >> checkLine xs
+       else case ss of
+              (z:zs) -> if mappingP z == x
+                           then put zs >> checkLine xs
+                           else return $ errorMap x
+              _ -> return $ errorMap x
 
 evalLine :: String -> Int
-evalLine xs = evalState checkLine (xs, "")
+evalLine xs = evalState (checkLine xs) ""
+
+execLine :: String -> String
+execLine xs = execState (checkLine xs) ""
 
 computeCost :: String -> Int
 computeCost = foldl (\acc x -> acc * 5 + x) 0 . map costMap
-
-getMiddleScore :: [Int] -> Int
-getMiddleScore xs = xs !! m
-    where m = length xs `div` 2
-
-execLine :: String -> String
-execLine xs = snd $ execState checkLine (xs, "")
 
 solution1 :: [String] -> Int
 solution1 = sum . map evalLine
 
 solution2 :: [String] -> Int
 solution2 = getMiddleScore . sort . map (computeCost . execLine) . filter ((==0) . evalLine)
+    where getMiddleScore xs = let m = length xs `div` 2 in xs !! m
 
 solve1 :: String -> Int
 solve1 = solution1 . parseContent
