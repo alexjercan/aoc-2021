f982fb36dd0c39e2f97af33459865aa68a0c441b
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-12-13 08:48:44 +0200

    added day 13

--- src/Day13.hs
+++ src/Day13.hs
@@ -1,13 +1,49 @@
 module Day13 where
 
-solve1 :: String -> Int
-solve1 content = undefined
+import qualified Data.Map as M
+import Data.List.Split (splitOn)
+import Control.Arrow ((&&&))
+import Data.List (nub, transpose)
 
-solve2 :: String -> Int
-solve2 content = undefined
+parseContent :: String -> ([(Int, Int)], [(Char, Int)])
+parseContent = (map parseDot . head &&& map parseFold . (!!1)) . splitOn [""] . lines
+
+parseDot :: String -> (Int, Int)
+parseDot = (read . head &&& read . (!!1)) . splitOn ","
+
+parseFold :: String -> (Char, Int)
+parseFold = (head . head &&& read . (!!1)) . splitOn "=" . (!!2) . words
+
+mark :: (Int, Int) -> [String] -> [String]
+mark (i, j) vs = up ++ [xs ++ ['#'] ++ ys] ++ down
+    where (up, l:down) = splitAt i vs
+          (xs, _:ys) = splitAt j l
+
+pprint :: [(Int, Int)] -> String
+pprint xs = unlines $ transpose $ foldl (flip mark) (replicate n $ replicate m  ' ') xs
+    where n = (+1) $ maximum $ map fst xs
+          m = (+1) $ maximum $ map snd xs
+
+foldDot :: (Char, Int) -> (Int, Int) -> (Int, Int)
+foldDot ('x', v) (x, y) = if x > v then (2 * v - x, y) else (x, y)
+foldDot ('y', v) (x, y) = if y > v then (x, 2 * v - y) else (x, y)
+foldDot _ _ = undefined
+
+solution1 :: [(Int, Int)] -> (Char, Int) -> Int
+solution1 b f = length $ nub $ map (foldDot f) b
+
+solution2 :: [(Int, Int)]-> [(Char, Int)] -> [(Int, Int)]
+solution2 = foldl (\acc f -> nub $ map (foldDot f) acc)
+
+solve1 :: String -> Int
+solve1 content = solution1 b (head fs)
+    where (b, fs) = parseContent content
+
+solve2' :: String -> [(Int, Int)]
+solve2' = uncurry solution2 . parseContent
 
 solve :: String -> IO ()
 solve content = do
     print $ solve1 content
-    print $ solve2 content
+    putStrLn $ pprint $ solve2' content
 
--- test/Day13Test.hs
+++ test/Day13Test.hs
@@ -5,15 +5,9 @@ module Day13Test where
 import Day13
 import Test.Framework
 
-sampleInput :: IO String
-sampleInput = readFile "input/day13/sample_input.txt"
+sampleInput :: String
+sampleInput = "6,10\n0,14\n9,10\n0,3\n10,4\n4,11\n6,0\n6,12\n4,1\n0,13\n10,12\n3,4\n3,0\n8,4\n1,10\n2,14\n8,10\n9,0\n\nfold along y=7\nfold along x=5\n"
 
 test_solve1 :: IO ()
 test_solve1 = do
-    content <- sampleInput
-    assertEqual 0 $ solve1 content
-
-test_solve2 :: IO ()
-test_solve2 = do
-    content <- sampleInput
-    assertEqual 0 $ solve2 content
+    assertEqual 17 $ solve1 sampleInput
