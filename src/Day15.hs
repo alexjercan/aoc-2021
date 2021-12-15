8ea788114f61065fe9cef297792a9cabe9bd1a86
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-12-15 14:09:48 +0200

    added dijkstra

--- src/Day15.hs
+++ src/Day15.hs
@@ -1,21 +1,28 @@
 module Day15 where
 
-import Control.Monad.State
-    ( forM_, modify, evalState, MonadState(put, get), State )
-import Data.Char (digitToInt)
-import qualified Data.Map as M
 import Control.Arrow ((&&&))
-import qualified Data.Set as S
-import Data.List (minimumBy)
+import Control.Monad.State
+    ( MonadState(get, put)
+    , State
+    , evalState
+    , execState
+    , forM_
+    , modify
+    )
+import Data.Char (digitToInt)
 import Data.Function (on)
-import Data.Maybe (fromMaybe, fromJust)
+import Data.List (minimumBy)
+import qualified Data.Map as M
+import Data.Maybe (fromJust, fromMaybe)
+import qualified Data.Set as S
 
 type Input = [[Int]]
 
 parseContent :: String -> Input
 parseContent = map (map digitToInt) . lines
 
-sampleInput = "1163751742\n1381373672\n2136511328\n3694931569\n7463417111\n1319128137\n1359912421\n3125421639\n1293138521\n2311944581\n"
+sampleInput =
+    "1163751742\n1381373672\n2136511328\n3694931569\n7463417111\n1319128137\n1359912421\n3125421639\n1293138521\n2311944581\n"
 
 wrap :: (Eq p, Num p) => p -> p
 wrap x
@@ -24,68 +31,99 @@ wrap x
 
 replicateCol :: (Eq t, Eq b, Num t, Num b) => t -> [[b]] -> [[b]]
 replicateCol 1 ms = ms
-replicateCol n ms = zipWith (++) ms $ replicateCol (n-1) (map (map wrap) ms)
+replicateCol n ms = zipWith (++) ms $ replicateCol (n - 1) (map (map wrap) ms)
 
 replicateMat :: (Eq b, Num b, Eq t, Num t) => t -> [[b]] -> [[b]]
 replicateMat n = go n
-    where go 1 ms = replicateCol n ms
-          go i ms = replicateCol n ms ++ go (i-1) (map (map wrap) ms)
-
-type MyState = (S.Set (Int, Int), M.Map (Int, Int) Int, M.Map (Int, Int) Int)
-type Eval = State MyState
-
-isValid :: Int -> Int -> (Int, Int) -> Bool
-isValid n m (i, j) = 0 <= i && i < n && 0 <= j && j < m
+  where
+    go 1 ms = replicateCol n ms
+    go i ms = replicateCol n ms ++ go (i - 1) (map (map wrap) ms)
 
 getNeighbors :: Int -> Int -> (Int, Int) -> [(Int, Int)]
-getNeighbors n m (i, j) = filter (isValid n m) [(i+1,j), (i-1,j), (i,j+1), (i,j-1)]
+getNeighbors n m (i, j) =
+    filter (isValid n m) [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]
+  where
+    isValid n m (i, j) = 0 <= i && i < n && 0 <= j && j < m
 
-handleNeighbor :: (Int, Int) -> ((Int, Int) -> Int) -> ((Int, Int) -> Int) -> (Int, Int) -> MyState -> MyState
-handleNeighbor current h c neighbor (openSet, gScore, fScore) = do
-    let g = gScore M.! current + c neighbor
-    if g < fromMaybe 9999 (gScore M.!? neighbor)
-       then do
-           let gScore' = M.insert neighbor g gScore
-           let fScore' = M.insert neighbor (g + h neighbor) fScore
-           let openSet' = S.insert neighbor openSet
-           (openSet', gScore', fScore')
+cost :: M.Map (Int, Int) Int -> (Int, Int) -> Int
+cost = (M.!)
+
+isTarget :: (Int, Int) -> (Int, Int) -> Bool
+isTarget = (==)
+
+answer :: (Int, Int) -> M.Map (Int, Int) Int -> Int
+answer = flip (M.!)
+
+type MyState = (S.Set (Int, Int), M.Map (Int, Int) Int)
+
+type Eval = State MyState
+
+minimumVertex :: Ord a => S.Set a -> M.Map a Int -> a
+minimumVertex q dist =
+    fst . minimumBy (compare `on` snd) $
+    filter ((`S.member` q) . fst) $ M.toList dist
+
+handleNeighbor ::
+       (Int, Int) -> ((Int, Int) -> Int) -> (Int, Int) -> MyState -> MyState
+handleNeighbor u c v (q, dist) = do
+    let alt = dist M.! u + c v
+    if alt < fromMaybe 9999 (dist M.!? v)
+        then (q, M.insert v alt dist)
+        else (q, dist)
+
+dijkstraM ::
+       ((Int, Int) -> Bool)
+    -> ((Int, Int) -> [(Int, Int)])
+    -> ((Int, Int) -> Int)
+    -> (M.Map (Int, Int) Int -> Int)
+    -> Eval Int
+dijkstraM isT neighs c a = do
+    (q, dist) <- get
+    if S.null q
+        then return $ a dist
         else do
-            (openSet, gScore, fScore)
+            let u = minimumVertex q dist
+            let q' = S.delete u q
+            put (q', dist)
+            if isT u
+                then return $ a dist
+                else do
+                    forM_ (neighs u) (modify . handleNeighbor u c)
+                    dijkstraM isT neighs c a
 
-astarM :: (Int, Int) -> ((Int, Int) -> [(Int, Int)]) -> ((Int, Int) -> Int) -> ((Int, Int) -> Int) -> Eval (Maybe Int)
-astarM g neighs h c = do
-    (openSet, gScore, fScore) <- get
-    if S.null openSet
-       then return Nothing
-       else do
-           let (current, f) = minimumBy (compare `on` snd) $ filter ((`S.member` openSet) . fst) $ M.toList fScore
-           if current == g
-                  then return $ Just f
-                  else do
-                      let openSet' = S.delete current openSet
-                      put (openSet', gScore, fScore)
-                      forM_ (neighs current) (modify . handleNeighbor current h c)
-                      astarM g neighs h c
+dijkstra ::
+       (Int, Int)
+    -> M.Map (Int, Int) a
+    -> ((Int, Int) -> Bool)
+    -> ((Int, Int) -> [(Int, Int)])
+    -> ((Int, Int) -> Int)
+    -> (M.Map (Int, Int) Int -> Int)
+    -> Int
+dijkstra source vs isT neighs c a =
+    evalState
+        (dijkstraM isT neighs c a)
+        (S.fromList $ M.keys vs, M.singleton source 0)
 
-manhattan :: (Int, Int) -> (Int, Int) -> Int
-manhattan (x1, y1) (x2, y2) = abs (y2 - y1) + abs (x2 - x1)
-
-cost :: [[Int]] -> (Int, Int) -> Int
-cost m (i, j) = m !! i !! j
-
-astar :: (Int, Int) -> (Int, Int) -> ((Int, Int) -> [(Int, Int)]) -> ((Int, Int) -> Int) -> ((Int, Int) -> Int) -> Maybe Int
-astar s g neighs h c = evalState (astarM g neighs h c) (S.singleton s, M.singleton s 0, M.singleton s $ h s)
-
-solution :: [[Int]] -> Maybe Int
-solution ms = astar (0, 0) (n-1, m-1) (getNeighbors n m) (manhattan (n-1, m-1)) (cost ms)
-    where n = length ms
-          m = length $ head ms
+solution :: [[Int]] -> Int
+solution ms =
+    dijkstra
+        (0, 0)
+        vs
+        (isTarget (n - 1, m - 1))
+        (getNeighbors n m)
+        (cost vs)
+        (answer (n - 1, m - 1))
+  where
+    n = length ms
+    m = length $ head ms
+    indices = [(i, j) | i <- [0 .. n - 1], j <- [0 .. m - 1]]
+    vs = M.fromList $ zip indices (map (\(i, j) -> ms !! i !! j) indices)
 
 solve1 :: [[Int]] -> Int
-solve1 = fromJust . solution
+solve1 = solution
 
 solve2 :: [[Int]] -> Int
-solve2 = fromJust . solution . replicateMat 5
+solve2 = solution . replicateMat 1
 
 solve :: String -> String
 solve = show . (solve1 &&& solve2) . parseContent
