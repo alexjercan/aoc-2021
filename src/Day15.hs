c0cbb975dec3fe1c64da95a9fd8a528ccace8325
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-12-15 09:36:52 +0200

    saved

--- src/Day15.hs
+++ src/Day15.hs
@@ -11,13 +11,16 @@ parseContent = map (map digitToInt) . lines
 
 sampleInput = "1163751742\n1381373672\n2136511328\n3694931569\n7463417111\n1319128137\n1359912421\n3125421639\n1293138521\n2311944581\n"
 
+wrap :: (Eq p, Num p) => p -> p
 wrap x
     | x == 9 = 1
     | otherwise = x + 1
 
+replicateCol :: (Eq t, Eq b, Num t, Num b) => t -> [[b]] -> [[b]]
 replicateCol 1 ms = ms
 replicateCol n ms = zipWith (++) ms $ replicateCol (n-1) (map (map wrap) ms)
 
+replicateMat :: (Eq b, Num b, Eq t, Num t) => t -> [[b]] -> [[b]]
 replicateMat n = go n
     where go 1 ms = replicateCol n ms
           go i ms = replicateCol n ms ++ go (i-1) (map (map wrap) ms)
@@ -30,17 +33,21 @@ initStep xs = M.fromList (zip ns row ++ zip ms col)
           col = scanl1 (+) $ map head xs
 
 dpStep :: Input -> M.Map (Int, Int) Int -> (Int, Int) -> M.Map (Int, Int) Int
-dpStep ms dp p@(i, j) = M.insert p (ms !! i !! j + min (dp M.! (i-1, j) ) (dp M.! (i, j-1))) dp
+dpStep ms dp p@(i, j) = M.insert p ((ms !! i !! j) + min (dp M.! (i-1, j)) (dp M.! (i, j-1))) dp
     where n = length ms
           m = length (head ms)
 
+solution :: [[Int]] -> Int
 solution xs = (dp' M.! (n-1,m-1)) - head (head xs)
     where n = length xs
           m = length (head xs)
           dp = initStep xs
           dp' =  foldl (dpStep xs) dp [(i, j) | i <- [1..n-1], j <- [1..m-1]]
 
+solve1 :: [[Int]] -> Int
 solve1 = solution
+
+solve2 :: [[Int]] -> Int
 solve2 = solution . replicateMat 5
 
 solve :: String -> String
