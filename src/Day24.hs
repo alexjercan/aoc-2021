e145bf8b680472adb8345469c857f7bf1d0871e0
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-12-24 15:32:18 +0200

    refactored day 24
    don't read the last commit message for this file

--- src/Day24.hs
+++ src/Day24.hs
@@ -5,16 +5,39 @@ import Data.List.Split (splitOn)
 
 parseContent :: String -> ([Integer], [Integer])
 parseContent = unzip . map g . tail . splitOn ["inp w"] . lines
-  where g gr = (read (words (gr !! 4) !! 2), read (words (gr !! 14) !! 2))
+  where
+    g gr = (read (words (gr !! 4) !! 2), read (words (gr !! 14) !! 2))
 
-accumulator :: ((Integer, Integer), [(Int, Integer)]) -> (Int, Integer, Integer) -> ((Integer, Integer), [(Int, Integer)])
+accumulator ::
+       ((Integer, Integer), [(Int, Integer)])
+    -> (Int, Integer, Integer)
+    -> ((Integer, Integer), [(Int, Integer)])
 accumulator ((p, q), stack) (i, a, b)
     | a > 0 = ((p, q), (i, b) : stack)
-    | otherwise = ((p - abs (a + b') * 10 ^ (13 - if a > (-b') then j else i) , q + abs (a + b') * 10 ^ (13 - if a < (-b') then j else i)) , stack')
-  where ((j, b'):stack') = stack
+    | otherwise =
+        ( ( p -
+            abs (a + b') *
+            10 ^
+            (13 -
+             if a > (-b')
+                 then j
+                 else i)
+          , q +
+            abs (a + b') *
+            10 ^
+            (13 -
+             if a < (-b')
+                 then j
+                 else i))
+        , stack')
+  where
+    ((j, b'):stack') = stack
 
 solution :: [Integer] -> [Integer] -> (Integer, Integer)
-solution as bs = fst $ foldl accumulator ((99999999999999, 11111111111111), []) $ zip3 [0 .. 13] as bs
+solution as bs =
+    fst $
+    foldl accumulator ((99999999999999, 11111111111111), []) $
+    zip3 [0 .. 13] as bs
 
 solve1 :: ([Integer], [Integer]) -> Integer
 solve1 = fst . uncurry solution
