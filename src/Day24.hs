c8d3c90d0ec33b4f33f081dab6820895bee5503c
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-12-24 15:27:40 +0200

    added day 24
    i copied the solution from a python dev bro. i am ashamed
    but wtf who would look at the input file. my initial solution
    had 200 lines and then i realised that the input range was of
    the order of 10 triliards or whatever. and my solution would take
    literally 10 million years to finish, so i opened up reddit and
    got spoiled by the fact that the input was a cycle, and out of
    all the instructions only 2 of them mattered for this problem
    the fourth one and the fifteenth one, then i saw a solution that
    took 10 lines of code in python and copied it, i still don't understand
    why a stack solves this problem. help. send help. i suck at coding

--- src/Day24.hs
+++ src/Day24.hs
@@ -3,27 +3,6 @@ module Day24 where
 import Control.Arrow (Arrow((&&&)))
 import Data.List.Split (splitOn)
 
--- we have 14 groups of instructions each  starting with "inp w"
--- inp w      W X Y Z
--- mul x 0    W 0 Y Z
--- add x z    W Z Y Z
--- mod x 26   W (Z mod 26) Y Z
--- div z a    W (Z mod 26) Y (Z div a)
--- add x b    W (Z mod 26 + b) Y (Z div a)
--- eql x w    W (W == Z mod 26 + b) Y (Z div a)
--- eql x 0    W (0 == (W == Z mod 26 + b)) Y (Z div a)
--- mul y 0    W (........................) 0 (Z div a)
--- add y 25   W (........................) 25 (Z div a)
--- mul y x    W (...) (25 * (...)) (Z div a)
--- add y 1    W (...) (1 + 25 * (...)) (Z div a)
--- mul z y    W (...) (1 + 25 * (...)) ((Z div a) * (1 + 25 * (...)))
--- mul y 0    W (...) 0 ((Z div a) * (1 + 25 * (...)))
--- add y w    W (...) W ((Z div a) * (1 + 25 * (...)))
--- add y c    W (...) (W + c) ((Z div a) * (1 + 25 * (...)))
--- mul y x    W (...) ((W + c) * (...)) ((Z div a) * (1 + 25 * (...)))
--- add z y    W (...) ((W + c) * (...)) ((W + c) * (...) + ((Z div a) * (1 + 25 * (...))))
--- Z' = (Z div a) + (0 == (W == Z mod 26 + b)) * (W + c + Z div a * 25)
-
 parseContent :: String -> ([Integer], [Integer])
 parseContent = unzip . map g . tail . splitOn ["inp w"] . lines
   where g gr = (read (words (gr !! 4) !! 2), read (words (gr !! 14) !! 2))
