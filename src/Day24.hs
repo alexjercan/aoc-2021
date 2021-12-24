a848feb7d489dd02e0ba07e8cad253c2b60a05b0
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-12-24 15:27:07 +0200

    wtf i am done this was so crazy
    -- we have 14 groups of instructions each  starting with "inp w"
    -- inp w      W X Y Z
    -- mul x 0    W 0 Y Z
    -- add x z    W Z Y Z
    -- mod x 26   W (Z mod 26) Y Z
    -- div z a    W (Z mod 26) Y (Z div a)
    -- add x b    W (Z mod 26 + b) Y (Z div a)
    -- eql x w    W (W == Z mod 26 + b) Y (Z div a)
    -- eql x 0    W (0 == (W == Z mod 26 + b)) Y (Z div a)
    -- mul y 0    W (........................) 0 (Z div a)
    -- add y 25   W (........................) 25 (Z div a)
    -- mul y x    W (...) (25 * (...)) (Z div a)
    -- add y 1    W (...) (1 + 25 * (...)) (Z div a)
    -- mul z y    W (...) (1 + 25 * (...)) ((Z div a) * (1 + 25 * (...)))
    -- mul y 0    W (...) 0 ((Z div a) * (1 + 25 * (...)))
    -- add y w    W (...) W ((Z div a) * (1 + 25 * (...)))
    -- add y c    W (...) (W + c) ((Z div a) * (1 + 25 * (...)))
    -- mul y x    W (...) ((W + c) * (...)) ((Z div a) * (1 + 25 * (...)))
    -- add z y    W (...) ((W + c) * (...)) ((W + c) * (...) + ((Z div a) * (1 + 25 * (...))))
    -- Z' = (Z div a) + (0 == (W == Z mod 26 + b)) * (W + c + Z div a * 25)

--- src/Day24.hs
+++ src/Day24.hs
@@ -1,180 +1,47 @@
-{-# LANGUAGE DeriveFunctor #-}
-
 module Day24 where
 
 import Control.Arrow (Arrow((&&&)))
-import Control.Monad.Except
-    ( ExceptT
-    , MonadError(catchError, throwError)
-    , runExceptT
-    , when
-    )
-import Control.Monad.Identity (Identity(runIdentity), IdentityT(runIdentityT))
-import Control.Monad.State
-    ( State
-    , StateT
-    , execState
-    , execStateT
-    , get
-    , modify
-    , put
-    )
-import Data.Bifunctor (Bifunctor(first))
-import qualified Data.Map as M
-import Text.Parsec ((<|>), char, space, string, try)
-import Util.Parser (Parser, integerP, parseList)
-import Data.List (inits)
+import Data.List.Split (splitOn)
 
-insertWithFrom :: Ord k => (a -> a -> a) -> k -> k -> M.Map k a -> M.Map k a
-insertWithFrom f d s m = M.insertWith f d (m M.! s) m
+-- we have 14 groups of instructions each  starting with "inp w"
+-- inp w      W X Y Z
+-- mul x 0    W 0 Y Z
+-- add x z    W Z Y Z
+-- mod x 26   W (Z mod 26) Y Z
+-- div z a    W (Z mod 26) Y (Z div a)
+-- add x b    W (Z mod 26 + b) Y (Z div a)
+-- eql x w    W (W == Z mod 26 + b) Y (Z div a)
+-- eql x 0    W (0 == (W == Z mod 26 + b)) Y (Z div a)
+-- mul y 0    W (........................) 0 (Z div a)
+-- add y 25   W (........................) 25 (Z div a)
+-- mul y x    W (...) (25 * (...)) (Z div a)
+-- add y 1    W (...) (1 + 25 * (...)) (Z div a)
+-- mul z y    W (...) (1 + 25 * (...)) ((Z div a) * (1 + 25 * (...)))
+-- mul y 0    W (...) 0 ((Z div a) * (1 + 25 * (...)))
+-- add y w    W (...) W ((Z div a) * (1 + 25 * (...)))
+-- add y c    W (...) (W + c) ((Z div a) * (1 + 25 * (...)))
+-- mul y x    W (...) ((W + c) * (...)) ((Z div a) * (1 + 25 * (...)))
+-- add z y    W (...) ((W + c) * (...)) ((W + c) * (...) + ((Z div a) * (1 + 25 * (...))))
+-- Z' = (Z div a) + (0 == (W == Z mod 26 + b)) * (W + c + Z div a * 25)
 
-eql :: Eq a => a -> a -> Integer
-eql a b =
-    if a == b
-        then 1
-        else 0
+parseContent :: String -> ([Integer], [Integer])
+parseContent = unzip . map g . tail . splitOn ["inp w"] . lines
+  where g gr = (read (words (gr !! 4) !! 2), read (words (gr !! 14) !! 2))
 
-toDigits :: Integer -> Ram
-toDigits 0 = []
-toDigits x = toDigits (x `div` 10) ++ [x `mod` 10]
+accumulator :: ((Integer, Integer), [(Int, Integer)]) -> (Int, Integer, Integer) -> ((Integer, Integer), [(Int, Integer)])
+accumulator ((p, q), stack) (i, a, b)
+    | a > 0 = ((p, q), (i, b) : stack)
+    | otherwise = ((p - abs (a + b') * 10 ^ (13 - if a > (-b') then j else i) , q + abs (a + b') * 10 ^ (13 - if a < (-b') then j else i)) , stack')
+  where ((j, b'):stack') = stack
 
-fromDigits :: Ram -> Integer
-fromDigits = foldl1 (\acc x -> acc * 10 + x)
+solution :: [Integer] -> [Integer] -> (Integer, Integer)
+solution as bs = fst $ foldl accumulator ((99999999999999, 11111111111111), []) $ zip3 [0 .. 13] as bs
 
-data Reg
-    = W
-    | X
-    | Y
-    | Z
-    deriving (Show, Ord, Eq)
+solve1 :: ([Integer], [Integer]) -> Integer
+solve1 = fst . uncurry solution
 
-data Arg
-    = Imm Integer
-    | Reg Reg
-    deriving (Show, Ord, Eq)
-
-data Ins a
-    = Inp Reg
-    | Add Reg a
-    | Mul Reg a
-    | Div Reg a
-    | Mod Reg a
-    | Eql Reg a
-    deriving (Show, Ord, Eq, Functor)
-
-type Instr = Ins Arg
-
-type ALU = M.Map Reg Integer
-
-type Ram = [Integer]
-
-isValid :: ALU -> Bool
-isValid = (== 0) . (M.! Z)
-
-initALU :: ALU
-initALU = M.fromList [(W, 0), (X, 0), (Y, 0), (Z, 0)]
-
-minRam :: Integer
-minRam = 11111111111111
-
-maxRam :: Integer
-maxRam = 99999999999999
-
-immP :: Parser Integer
-immP = toInteger <$> integerP
-
-regP :: Parser Reg
-regP = W <$ char 'w' <|> X <$ char 'x' <|> Y <$ char 'y' <|> Z <$ char 'z'
-
-argP :: Parser Arg
-argP = Imm <$> immP <|> Reg <$> regP
-
-inpP :: Parser Instr
-inpP = Inp <$> (string "inp" >> space >> regP)
-
-addP :: Parser Instr
-addP = Add <$> (string "add" >> space >> regP) <*> (space >> argP)
-
-mulModP :: Parser Instr
-mulModP =
-    char 'm' >>
-    (Mul <$> (string "ul" >> space >> regP) <*> (space >> argP) <|>
-     Mod <$> (string "od" >> space >> regP) <*> (space >> argP))
-
-divP :: Parser Instr
-divP = Div <$> (string "div" >> space >> regP) <*> (space >> argP)
-
-eqlP :: Parser Instr
-eqlP = Eql <$> (string "eql" >> space >> regP) <*> (space >> argP)
-
-instrP :: Parser Instr
-instrP = inpP <|> addP <|> mulModP <|> divP <|> eqlP
-
-type Input = [Instr]
-
-parseContent :: String -> Input
-parseContent = parseList instrP . lines
-
-type Eval = StateT (ALU, Ram) (ExceptT String Identity)
-
-evalM :: Instr -> Eval ()
-evalM (Inp r) = modify (\(alu, x:xs) -> (M.insert r x alu, xs))
-evalM (Add d (Reg s)) = modify (first (insertWithFrom (+) d s))
-evalM (Add d (Imm s)) = modify (first (M.insertWith (+) d s))
-evalM (Mul d (Reg s)) = modify (first (insertWithFrom (*) d s))
-evalM (Mul d (Imm s)) = modify (first (M.insertWith (*) d s))
-evalM (Div d (Reg s)) =
-    get >>=
-    (\(alu, _) ->
-         if alu M.! s == 0
-             then throwError "Division by Zero"
-             else modify (first (insertWithFrom (flip quot) d s)))
-evalM (Div d (Imm s)) =
-    get >>=
-    (\(alu, _) ->
-         if s == 0
-             then throwError "Division by Zero"
-             else modify (first (M.insertWith (flip quot) d s)))
-evalM (Mod d (Reg s)) =
-    get >>=
-    (\(alu, _) ->
-         if alu M.! s <= 0 || alu M.! d < 0
-             then throwError "Division by Zero"
-             else modify (first (insertWithFrom (flip rem) d s)))
-evalM (Mod d (Imm s)) =
-    get >>=
-    (\(alu, _) ->
-         if s <= 0 || alu M.! d < 0
-             then throwError "Division by Zero"
-             else modify (first (M.insertWith (flip rem) d s)))
-evalM (Eql d (Reg s)) = modify (first (insertWithFrom eql d s))
-evalM (Eql d (Imm s)) = modify (first (M.insertWith eql d s))
-
-evalListM :: [Instr] -> Eval ()
-evalListM = mapM_ evalM
-
-checkRam xs ram =
-    case h of
-        Left _ -> False
-        Right (alu, _) -> isValid alu
-  where
-    f = execStateT (evalListM xs) (initALU, ram)
-    g = runExceptT f
-    h = runIdentity g
-
-logSearch :: Integer -> Integer -> [Instr] -> ()
-logSearch lo hi xs = do
-    let mid = (lo + hi) `div` 2
-    let ok = checkRam xs (toDigits mid)
-    if not ok
-       then logSearch (lo+1) mid xs
-       else logSearch mid maxRam xs
-
--- solution xs = fromDigits $ head $ dropWhile (not . bruteForce xs) ramPermutations
-
-solve1 = id -- map (\x -> bruteForce x (head ramPermutations)) . inits
-
-solve2 = const ()
+solve2 :: ([Integer], [Integer]) -> Integer
+solve2 = snd . uncurry solution
 
 solve :: String -> String
 solve = show . (solve1 &&& solve2) . parseContent
